543e933724548ec2973a02dd147254972786af98
lgferreiracic <lgferreira.cic@uesc.br>
2026-09-18 19:37:19 -0300

    First commit

---
+++ mandelbrot_sequencial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+
+//Função que mede o tempo em segundos
+double get_time()
+{
+
+}
+
+int mandelbrot(int x, int y, int width, int height, int max_iter, double re_min, double re_max, double im_min, double im_max)
+{
+    
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
