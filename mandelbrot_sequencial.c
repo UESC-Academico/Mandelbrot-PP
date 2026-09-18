a32bb433a5f6545963f879e52f5ba207dec7204d
lgferreiracic <lgferreira.cic@uesc.br>
2026-09-18 20:24:30 -0300

    feat: função de mandelbrot, geração da imagem e gitignore

--- mandelbrot_sequencial.c
+++ mandelbrot_sequencial.c
@@ -11,9 +11,25 @@ double get_time()
     return tv.tv_sec + tv.tv_usec * 1e-6;
 }
 
+// Função que calcula o número de iterações para o conjunto de Mandelbrot
 int mandelbrot(int x, int y, int width, int height, int max_iter, double re_min, double re_max, double im_min, double im_max)
 {
-    
+    double cReal = re_min + ((re_max - re_min) * x) / width;
+    double cImaginary = im_min + ((im_max - im_min) * y) / height;
+
+    double zReal = 0.0, zImaginary = 0.0;
+    double zReal2 = 0.0, zImaginary2 = 0.0;
+    int n = 0;
+
+    while ((zReal2 + zImaginary2 <= 4.0) && (n < max_iter)) 
+    {
+        zImaginary = 2.0 * zReal * zImaginary + cImaginary;
+        zReal = zReal2 - zImaginary2 + cReal;
+        zReal2 = zReal * zReal;
+        zImaginary2 = zImaginary * zImaginary;
+        n++;
+    }
+    return n;
 }
 
 int main(int argc, char *argv[])
@@ -27,7 +43,8 @@ int main(int argc, char *argv[])
 
     // Alocação de memória para a matriz de pixels
     int *matriz = (int *)malloc((size_t)width * height * sizeof(int));
-    if (!matriz) {
+    if (!matriz) 
+    {
         printf("Erro de alocacao de memoria.\n");
         return 1;
     }
@@ -36,22 +53,43 @@ int main(int argc, char *argv[])
     double t_inicio = get_time();
 
     // Cálculo do conjunto de Mandelbrot
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) 
+    {
+        for (int x = 0; x < width; x++) 
+        {
             matriz[y * width + x] = mandelbrot(x, y, width, height, max_iter, re_min, re_max, im_min, im_max);
         }
     }
 
     // Fim do cronômetro
     double t_fim = get_time();
-    printf("Tempo de calculo: %.4f segundos\n", t_fim - t_inicio);
 
     // Salvar Arquivo Binário
     FILE *fbin = fopen("mandelbrot_matriz.bin", "wb");
-    if (fbin) {
+    if (fbin) 
+    {
         fwrite(matriz, sizeof(int), (size_t)width * height, fbin);
         fclose(fbin);
     }
+
+    // Salvar imagem no formato PPM
+    FILE *fppm = fopen("mandelbrot.ppm", "w");
+    if (fppm) 
+    {
+        fprintf(fppm, "P3\n%d %d\n255\n", width, height);
+        for (int i = 0; i < width * height; i++) 
+        {
+            int n = matriz[i];
+            int r = (n * 2) % 256;
+            int g = (n * 4) % 256;
+            int b = (n * 13) % 256;
+            if (n == max_iter) { r = g = b = 0; }
+            fprintf(fppm, "%d %d %d ", r, g, b);
+        }
+        fclose(fppm);
+    }
+
+    free(matriz);
 
     return 0;
 }
