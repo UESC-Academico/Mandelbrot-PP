9b4f8fe8994338d8bf0b76d2d5d5284df66eb126
Laisaf173 <laisafranca890@hotmail.com>
2026-09-18 19:57:14 -0300

    feat: parâmetros, alocação e cálculo do mandelbrot

--- mandelbrot_sequencial.c
+++ mandelbrot_sequencial.c
@@ -18,5 +18,40 @@ int mandelbrot(int x, int y, int width, int height, int max_iter, double re_min,
 
 int main(int argc, char *argv[])
 {
+    // Parâmetros do conjunto de Mandelbrot
+    int width = 4096;
+    int height = 4096;
+    int max_iter = 1000;
+    double re_min = -2.0, re_max = 1.0;
+    double im_min = -1.5, im_max = 1.5;
+
+    // Alocação de memória para a matriz de pixels
+    int *matriz = (int *)malloc((size_t)width * height * sizeof(int));
+    if (!matriz) {
+        printf("Erro de alocacao de memoria.\n");
+        return 1;
+    }
+
+    // Início do cronômetro 
+    double t_inicio = get_time();
+
+    // Cálculo do conjunto de Mandelbrot
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            matriz[y * width + x] = mandelbrot(x, y, width, height, max_iter, re_min, re_max, im_min, im_max);
+        }
+    }
+
+    // Fim do cronômetro
+    double t_fim = get_time();
+    printf("Tempo de calculo: %.4f segundos\n", t_fim - t_inicio);
+
+    // Salvar Arquivo Binário
+    FILE *fbin = fopen("mandelbrot_matriz.bin", "wb");
+    if (fbin) {
+        fwrite(matriz, sizeof(int), (size_t)width * height, fbin);
+        fclose(fbin);
+    }
+
     return 0;
 }
