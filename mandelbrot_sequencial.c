2e661b7bd0d5a65906a0764c786bcda7de937690
lgferreiracic <lgferreira.cic@uesc.br>
2026-09-18 19:40:25 -0300

    feat: implementação da função get_time

--- mandelbrot_sequencial.c
+++ mandelbrot_sequencial.c
@@ -6,7 +6,9 @@
 //Função que mede o tempo em segundos
 double get_time()
 {
-
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
 }
 
 int mandelbrot(int x, int y, int width, int height, int max_iter, double re_min, double re_max, double im_min, double im_max)
